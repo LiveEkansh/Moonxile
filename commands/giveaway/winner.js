f77ae3e217d388eba2cedbdd573819c0979a7682
Darshan2208 <darshan310268@gmail.com>
2021-06-30 18:43:33 +0530

    Update winner.js

--- commands/giveaway/winner.js
+++ commands/giveaway/winner.js
@@ -11,12 +11,16 @@ module.exports = {
             return message.reply('Incorrect Usage : `;;winner @user`')
         };
 
-        message.channel.send(`╭<:1_tada:857846692233478154> **[${member}] won!** you may ask them if we're **LEGIT**.
-        **・━━━━━━━━━━━━━━━━━━━━━━━━━━━・**
-        <:1_info:857846561714208819> **PRO TIP!**
-        <:1_dots:857846478587428904> Be quick coming to our giveaway channels because we often delete the invite for the requirement after a few minutes, putting us above all other servers will allow you to be notified much easier!
-        <:1_dots:857846478587428904> <#857633375543885884> for +3s claim time!
-        **・━━━━━━━━━━━━━━━━━━━━━━━━━━━・**
-        ╰<:1_sign:857846611397050369> Stay in **Moonxile** for more!`)
+        message.channel.send(
+            [
+            `╭<:1_tada:857846692233478154> **[${member}] won!** you may ask them if we're **LEGIT**.`, 
+        `**・━━━━━━━━━━━━━━━━━━━━━━━━━━━・**`, 
+        `<:1_info:857846561714208819> **PRO TIP!**`,   
+        `<:1_dots:857846478587428904> Be quick coming to our giveaway channels because we often delete the invite for the requirement after a few minutes, putting us above all other servers will allow you to be notified much easier!`,               
+        `<:1_dots:857846478587428904> <#857633375543885884> for +3s claim time!`,
+        `**・━━━━━━━━━━━━━━━━━━━━━━━━━━━・**`,
+        `╰<:1_sign:857846611397050369> Stay in **Moonxile** for more!`
+    ]
+        )
     }
 }
