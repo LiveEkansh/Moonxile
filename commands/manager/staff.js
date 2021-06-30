1afc47a28c28234071e248c6263475af7e04afeb
Darshan2208 <darshan310268@gmail.com>
2021-06-30 19:16:43 +0530

    Update staff.js

--- commands/manager/staff.js
+++ commands/manager/staff.js
@@ -7,20 +7,22 @@ module.exports = {
 
         const embed = new Discord.MessageEmbed()
         .setAuthor(message.guild.name + ' Staff', message.guild.iconURL())
-        
-        .addField('୨・Creators', `${message.guild.roles.cache.find(r => r.name == '୨・Creators').members.map(m => m.user.tag).join('\n')}`, true)
-        .addField('୨・founder', `${message.guild.roles.cache.find(r => r.name == '୨・founder').members.map(m => m.user.tag).join('\n')}`, true)
-        .addField('୨・co founder', `${message.guild.roles.cache.find(r => r.name == '୨・co founder').members.map(m => m.user.tag).join('\n')}`, true)
 
-        .addField('୨・owner', `${message.guild.roles.cache.find(r => r.name == '୨・owner').members.map(m => m.user.tag).join('\n')}`, true)
-        .addField('୨・co owner', `${message.guild.roles.cache.find(r => r.name == '୨・co owner').members.map(m => m.user.tag).join('\n')}`, true)
-        .addField('୨・head admin', `${message.guild.roles.cache.find(r => r.name == '୨・head admin').members.map(m => m.user.tag).join('\n')}`, true)
-        
-        .addField('୨・admin', `${message.guild.roles.cache.find(r => r.name == '୨・admin').members.map(m => m.user.tag).join('\n')}`, true)
-        .addField('୨・head mod', `${message.guild.roles.cache.find(r => r.name == '୨・head mod').members.map(m => m.user.tag).join('\n')}`, true)
-        .addField('୨・moderator', `${message.guild.roles.cache.find(r => r.name == '୨・moderator').members.map(m => m.user.tag).join('\n')}`, true)
-        
-        .addField('୨・trial mod', `${message.guild.roles.cache.find(r => r.name == '୨・trial mod').members.map(m => m.user.tag).join('\n')}`)
+        .addFields(
+            {name: '୨・Creators', value: message.guild.roles.cache.find(r => r.name == '୨・Creators').members.map(m => m.user.tag).join('\n'), inline:true},
+            {name: '୨・founder', value: message.guild.roles.cache.find(r => r.name == '୨・founder').members.map(m => m.user.tag).join('\n'), inline:true},
+            {name: '୨・co founder', value: message.guild.roles.cache.find(r => r.name == '୨・co founder').members.map(m => m.user.tag).join('\n'), inline:true},
+
+            {name: '୨・owner', value: message.guild.roles.cache.find(r => r.name == '୨・owner').members.map(m => m.user.tag).join('\n'), inline:true},
+            {name: '୨・co owner', value: message.guild.roles.cache.find(r =>r.name == '୨・co owner').members.map(m => m.user.tag).join('\n'), inline:true},
+            {name: '୨・head admin', value: message.guild.roles.cache.find(r => r.name == '୨・head admin').members.map(m => m.user.tag).join('\n'), inline:true},
+
+            {name: '୨・admin', value: message.guild.roles.cache.find(r => r.name == '୨・admin').members.map(m => m.user.tag).join('\n'), inline:true},
+            {name: '୨・head mod', value: message.guild.roles.cache.find(r => r.name == '୨・head mod').members.map(m => m.user.tag).join('\n'), inline:true},
+            {name: '୨・moderator', value: message.guild.roles.cache.find(r => r.name == '୨・moderator').members.map(m => m.user.tag).join('\n'), inline:true},
+
+            {name: '୨・trial mod', value: message.guild.roles.cache.find(r => r.name == '୨・trial mod').members.map(m => m.user.tag).join('\n'), inline:true}
+        )
 
         .setColor('00FFCC')
         .setFooter('Last Updated')
