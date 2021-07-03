5da3cdbf21f2db31251c1b1e54b7e112c607f853
Darshan2208 <darshan310268@gmail.com>
2021-07-03 20:31:43 +0530

    Update purge.js

--- commands/manager/purge.js
+++ commands/manager/purge.js
@@ -1,42 +1,61 @@
 module.exports = {
     name: 'purge',
     args: true,
-    usage: '<number>',
+    usage: '<@user> <number> `or` <@user>',
     async execute(client, message, args, Discord){
-        if(!message.member.hasPermission('MANAGE_MESSAGES')){
-            return message.reply('Missing Permissions `MANAGE_MESSAGES`')
-        };
-        const num = parseInt(args[0]);
-        if(num < 1 || num > 100){
-            return message.channel.send('Cannot clear messages less than `1` and more than `100`')
-        };
+        const user = message.mentions.user.first();
+        const number = !!parseInt(args[0]) ? parseInt(args[0]) : parseInt(args[1]);
 
-        if(isNaN(num)){
-            return message.channel.send('Enter a valid number! Eg : `;;purge 10`')
-        };
+        const num = number + 1;
+        
+        await message.channel.messages.fetch({
+            limit: num,
+        }).then((messages) =>{
+            if(user){
+                const purge = user ? user.id : client.user.id;
 
-        const number = num + 1;
+                messages = messages.filter(m => m.author.id === purge).array().slice(0, num)
+            }
 
-        await message.channel.bulkDelete(
-            (await message.channel.messages.fetch({ limit: number }))
-            .filter(m => !m.pinned)
-        ).catch(console.error)
+        message.channel.bulkDelete(messages).catch(console.error)
+        });
 
-        message.channel.send(`<a:mx_check:858745251305226270> \`${num}\` message(s) cleared!`)
-        .then(message =>{
-            message.delete({ timeout: 3000 })
-        })
-        .catch(console.error);
+        await message.channel.send(`<a:mx_check:858745251305226270> | \`${number}\` message(s) cleared!`).then(m =>{
+            m.delete({timeout: 3000})
+        });
     }
-}
-// message.channel.bulkDelete(parseInt(number), true)
-// .catch(console.error);
-/*
-message.channel.fetchMessages({ limit: 100 })
-  .then(fetched => {
-    const notPinned = fetched.filter(fetchedMsg => !fetchedMsg.pinned);
+} // new updated code.
 
-    message.channel.bulkDelete(notPinned, true);
-  })
-  .catch(console.error);
-*/
+
+/**Ignoring old code. */
+// module.exports = {
+//     name: 'purge',
+//     args: true,
+//     usage: '<number>',
+//     async execute(client, message, args, Discord){
+//         if(!message.member.hasPermission('MANAGE_MESSAGES')){
+//             return message.reply('Missing Permissions `MANAGE_MESSAGES`')
+//         };
+//         const num = parseInt(args[0]);
+//         if(num < 1 || num > 100){
+//             return message.channel.send('Cannot clear messages less than `1` and more than `100`')
+//         };
+
+//         if(isNaN(num)){
+//             return message.channel.send('Enter a valid number! Eg : `;;purge 10`')
+//         };
+
+//         const number = num + 1;
+
+//         await message.channel.bulkDelete(
+//             (await message.channel.messages.fetch({ limit: number }))
+//             .filter(m => !m.pinned)
+//         ).catch(console.error)
+
+//         message.channel.send(`<a:mx_check:858745251305226270> \`${num}\` message(s) cleared!`)
+//         .then(message =>{
+//             message.delete({ timeout: 3000 })
+//         })
+//         .catch(console.error);
+//     }
+// }
