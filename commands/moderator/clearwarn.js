28d1f9835f15945bf9949aa269c6c92ec0c4e5af
Darshan2208 <darshan310268@gmail.com>
2021-07-04 20:53:35 +0530

    Update clearwarn.js

--- commands/moderator/clearwarn.js
+++ commands/moderator/clearwarn.js
@@ -3,7 +3,9 @@ const db = require('../models/warnings');
 module.exports = {
     name: 'clearwarn',
     async execute(client, message, args, Discord){
-        if(!message.member.hasPermission('MANAGE_ROLES'))
+        if(!message.member.hasPermission('MANAGE_ROLES') && !message.member.roles.cache.find(r => r.name === '୨・trial mod')){
+            return message.channel.send('Must have `@୨・trial mod` or above to execute this command')
+        };
         const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         if(!user) return message.channel.send('User not found.')
         await db.findOne({ guildid : message.guild.id, user: user.user.id}, async(err,data) => {
