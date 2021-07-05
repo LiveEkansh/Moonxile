cca6826c92d46907b29b63177b750e3e4b214734
Darshan2208 <darshan310268@gmail.com>
2021-07-05 10:38:37 +0530

    Update claims.js

--- commands/server/claims.js
+++ commands/server/claims.js
@@ -12,18 +12,25 @@ module.exports = {
         db.findOne({ guildid: message.guild.id, user: member.user.id }, async(err, data) =>{
             if(err) throw err;
             if(data) {
-                message.channel.send(new Discord.MessageEmbed()
+                
+                const embed = new Discord.MessageEmbed()
                     .setTitle(`${member.user.tag}'s Claims`)
-                    .setDescription(
-                        data.content.map(
-                            (w, i) => 
-                            `\`${i + 1}\` | **${w.date}**\nReward : **${w.reward}**\n`
-                        )
-                    )
+                    // .setDescription(
+                    //     data.content.map(
+                    //         (w, i) => 
+                    //         `\`${i + 1}\` | **${w.date}**\nReward : **${w.reward}**\n`
+                    //     )
+                    // )
                     .setColor("00ffcc")
                     .setFooter(`${data.content.length} claim(s)`, message.guild.iconURL({ dynamic: true }))
                     .setThumbnail(member.user.displayAvatarURL( {dynamic: true} ))
-                )
+
+                    const claims = data.content.map((w, i) =>{
+                        for(claim of claims){
+                            embed.addField(`${w.date}`, `Reward: **${w.reward}**\nClaim ID: \`${i + 1}\`\n`)
+                        }
+                    });
+                message.channel.send(embed)
             } else if (!data){
                 message.channel.send(new Discord.MessageEmbed()
                 .setTitle(`${member.user.tag}'s Claims`)
