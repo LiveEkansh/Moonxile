5af7818a6187c9406f3b83b4b18ead37e7a90ad4
Darshan2208 <darshan310268@gmail.com>
2021-07-06 17:26:23 +0530

    Fixed Greet

--- commands/models/welcome.js
+++ commands/models/welcome.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 let Schema = new mongoose.Schema({
     Guild: String,
     Channel: String,
+    Message: String
 });
 
 module.exports = mongoose.model("welcome", Schema);
