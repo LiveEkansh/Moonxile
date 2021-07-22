5926bf9eca04f4de570999fc4df31b1b3135b3fe
Darshan2208 <darshan310268@gmail.com>
2021-07-22 14:05:47 +0530

    Update list.js

--- commands/utility/list.js
+++ commands/utility/list.js
@@ -14,25 +14,26 @@ module.exports = {
     async execute(client, message, args, Discord){
         if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('Missing Permissions `ADMINISTRATOR`');
         const list = args[0];
+        if(!list) return message.lineReply(`Incorrect Usage, \`${prefix}list <supporters|donators|boosters>\``);
         if(list.toLowerCase() === 'supporters'){
             const members = message.guild.roles.cache.find(r => r.name === '୧🌱・supporter').members.map(m => m.user.tag).join('\n');
             message.lineReplyNoMention(new MessageEmbed()
                 .setAuthor(`${message.guild.name} supporters`, message.guild.iconURL())
-                .setDescription(members)
+                .setDescription(`${members}\n** **`)
                 .setColor('9933FF')
             )
         } else if (list.toLowerCase() === 'donators'){
             const members = message.guild.roles.cache.find(r => r.name === '୧🍂・donator').members.map(m => m.user.tag).join('\n');
             message.lineReplyNoMention(new MessageEmbed()
                 .setAuthor(`${message.guild.name} donators`, message.guild.iconURL())
-                .setDescription(members)
+                .setDescription(`${members}\n** **`)
                 .setColor('9933FF')
             )
         } else if (list.toLowerCase() === 'boosters'){
             const members = message.guild.roles.cache.find(r => r.name === '୧💐・booster').members.map(m => m.user.tag).join('\n');
             message.lineReplyNoMention(new MessageEmbed()
                 .setAuthor(`${message.guild.name} boosters`, message.guild.iconURL())
-                .setDescription(members)
+                .setDescription(`${members}\n** **`)
                 .setColor('9933FF')
             )
         }
